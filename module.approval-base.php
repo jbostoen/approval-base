1124d5af80168b82607b358e217e11d5a9071eee
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-01-17 08:51:10 +0000

    Preparing 2.4.1 : update trunk modules versions
    SVN:trunk[6389]

--- module.approval-base.php
+++ module.approval-base.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'approval-base/2.5.3',
+	'approval-base/2.5.4',
 	array(
 		// Identification
 		//
@@ -38,7 +38,7 @@ SetupWebPage::AddModule(
 			'model.approval-base.php'
 		),
 		'webservice' => array(
-			
+
 		),
 		'data.struct' => array(
 			// add your 'structure' definition XML files here,
@@ -46,11 +46,11 @@ SetupWebPage::AddModule(
 		'data.sample' => array(
 			// add your sample data XML files here,
 		),
-		
+
 		// Documentation
 		//
 		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
-		'doc.more_information' => '', // hyperlink to more information, if any 
+		'doc.more_information' => '', // hyperlink to more information, if any
 
 		// Default settings
 		//
