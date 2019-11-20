jbostoen/approval-base

commit 6abd619e73f8d08135dcf4c7be0c81b77c3e3dd6

N°2386: Fix dependencies for Symfony portal and libraries autoloader

main.approval-base.php
@@ -34,7 +34,7 @@ if (is_dir(MODULESROOT.'itop-portal-base'))
 	// If the portal is installed, then it may use the approval brick
 	// Important: The brick files are required manually and not autoloaded because it would not work as they are in the same namespace than the standard bricks but not in the same directory.
 	// We wanted to keep the same namespace to simplify future refactoring of this brick with the standard ones.
-	require_once APPROOT.'/lib/composer-vendor/autoload.php';
+	require_once APPROOT.'/lib/autoload.php';
 	require_once MODULESROOT.'approval-base/portal/src/Brick/ApprovalBrick.php';
 	require_once MODULESROOT.'approval-base/portal/src/Controller/ApprovalBrickController.php';
 	require_once MODULESROOT.'approval-base/portal/src/Router/ApprovalBrickRouter.php';

module.approval-base.php
@@ -32,6 +32,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
+			'itop-portal-base/2.7.0' 
 		),
 		'mandatory' => false,
 		'visible' => false,

commit aa201fd9de431a29744f0e1e944b3958c31529b1

N°2386 Rollback regression introduced in 6abd619e forcing to have the enhanced portal installed

main.approval-base.php
@@ -16,8 +16,6 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- *
- *
  */
 
 /**
@@ -32,9 +30,12 @@
 if (is_dir(MODULESROOT.'itop-portal-base'))
 {
 	// If the portal is installed, then it may use the approval brick
-	// Important: The brick files are required manually and not autoloaded because it would not work as they are in the same namespace than the standard bricks but not in the same directory.
+	// Important: The approval brick files are required manually and not autoloaded because it would not work as they are in the same namespace than the standard bricks but not in the same directory.
 	// We wanted to keep the same namespace to simplify future refactoring of this brick with the standard ones.
-	require_once APPROOT.'/lib/autoload.php';
+	require_once APPROOT.'lib/autoload.php';
+	// Note: This one has to be loaded here because the approval-base module is loaded BEFORE itop-portal-base
+	// Therefore, when it tries to load the PortalBrick, AbstractController, ... classes, it would not work.
+	require_once MODULESROOT.'itop-portal-base/portal/vendor/autoload.php';
 	require_once MODULESROOT.'approval-base/portal/src/Brick/ApprovalBrick.php';
 	require_once MODULESROOT.'approval-base/portal/src/Controller/ApprovalBrickController.php';
 	require_once MODULESROOT.'approval-base/portal/src/Router/ApprovalBrickRouter.php';

module.approval-base.php
@@ -15,8 +15,6 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- *
- *
  */
 
 /** @noinspection PhpUnhandledExceptionInspection */
@@ -32,7 +30,6 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			'itop-portal-base/2.7.0' 
 		),
 		'mandatory' => false,
 		'visible' => false,